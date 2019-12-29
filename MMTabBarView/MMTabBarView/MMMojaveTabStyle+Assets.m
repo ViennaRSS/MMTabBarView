ef6904d959f8625fd747ecb6c02545f2c2f2518b
shhyou <shu-hung.you@eecs.northwestern.edu>
2019-12-28 18:57:30 -0600

    Use secondaryLabelColor for inactive dark mode tab
    Cherry picked from 26fe2d0dbf7c6119b1a930162cebeddf313dc816

--- MMTabBarView/MMTabBarView/MMMojaveTabStyle+Assets.m
+++ MMTabBarView/MMTabBarView/MMMojaveTabStyle+Assets.m
@@ -85,6 +85,17 @@ inline static NSColor* labelColor(void)
 	return NSColor.textColor;
 }
 
+inline static NSColor* secondaryLabelColor(void)
+{
+#if __MAC_OS_X_VERSION_MAX_ALLOWED >= 101400
+	if ( @available(macos 10.10, *) )
+	{
+		return NSColor.secondaryLabelColor;
+	}
+#endif
+	return NSColor.selectedTextBackgroundColor;
+}
+
 - (NSDictionary<NSNumber*, NSDictionary<NSNumber*, id>*> *)assets
 {
 
@@ -212,8 +223,8 @@ inline static NSColor* labelColor(void)
                      @(MMMtabUnselectedHover)     : [NSColor colorWithSRGBRed:0.110 green:0.110 blue:0.110 alpha:1.0],
 
                      @(MMMtabSelectedFont)        : NSColor.textColor,
-                     @(MMMtabUnselectedFont)      : NSColor.disabledControlTextColor,
-                     @(MMMtabUnselectedHoverFont) : NSColor.textColor,
+                     @(MMMtabUnselectedFont)      : secondaryLabelColor(),
+                     @(MMMtabUnselectedHoverFont) : secondaryLabelColor(),
                      
                      @(MMMtabBarBackground)   : [NSColor colorWithSRGBRed:0.112 green:0.112 blue:0.112 alpha:1.0],
 
@@ -268,8 +279,8 @@ inline static NSColor* labelColor(void)
                      @(MMMtabUnselectedHover)     : [NSColor colorWithSRGBRed:0.110 green:0.110 blue:0.110 alpha:1.0],
 
                      @(MMMtabSelectedFont)        : NSColor.textColor,
-                     @(MMMtabUnselectedFont)      : NSColor.disabledControlTextColor,
-                     @(MMMtabUnselectedHoverFont) : NSColor.textColor,
+                     @(MMMtabUnselectedFont)      : secondaryLabelColor(),
+                     @(MMMtabUnselectedHoverFont) : secondaryLabelColor(),
                      
 					 @(MMMtabBarBackground)   : [NSColor colorWithSRGBRed:0.112 green:0.112 blue:0.112 alpha:1.0],
 
