881557335249d2dd4b76d5591ecbdbe5008e81ac
Eitot <Eitot@users.noreply.github.com>
2020-11-28 06:07:52 +0100

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "MMTabBarView",
+    defaultLocalization: LanguageTag("en"),
+    products: [
+        .library(name: "MMTabBarView", targets: ["MMTabBarView"]),
+    ],
+    targets: [
+        .target(
+            name: "MMTabBarView",
+            path: "MMTabBarView/MMTabBarView",
+            exclude: [
+                "MMTabBarView-Info.plist",
+                "MMTabBarView-Prefix.pch",
+            ],
+            resources: [
+                .process("en.lproj"),
+                .process("Images.xcassets"),
+                .process("Safari.xcassets"),
+                .process("Sierra.xcassets"),
+                .process("Yosemite.xcassets"),
+            ],
+            cSettings: [
+                .headerSearchPath(".")
+            ])
+    ]
+)
